3b45d46029dba05502b9fe819e5f94fec280873e
s0h3ck <s0h3ck@gmail.com>
2015-03-06 17:35:36 -0500

    06/03/2015

--- rew/lua/01_Getting/dofile_test.lua
+++
@@ -1,3 +0,0 @@
-dofile("lib1.lua") -- load your library
-n = norm(math.sqrt(2), math.sqrt(2))
-print(twice(n));
--- rew/lua/01_Getting/nil_value_test.lua
+++
@@ -1,6 +0,0 @@
-print(b) --> nil
-b = 10
-print(b) --> 10
-
-b = nil  --> delete the global variable
-print(b) --> nil
